e652a28a4fd2cf285a2a62aa337e43bf1a3e1ee5
codingniket <niketbachhawat7@gmail.com>
2025-10-02 22:10:24 +0530

    Authentication

---
+++ app/(root)/page.tsx
@@ -0,0 +1,28 @@
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const Home = async () => {
+  const session = await auth();
+
+  // console.log(session);
+
+  return (
+    <div>
+      <h1 className="bg-amber-500">Welcome to the world of Next.js</h1>
+
+      <form
+        className="px-10"
+        action={async () => {
+          "use server";
+
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Log out</Button>
+      </form>
+    </div>
+  );
+};
+
+export default Home;
