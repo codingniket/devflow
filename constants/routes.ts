b77240239aae02893b45fdb0f0164844aa97058e
codingniket <niketbachhawat7@gmail.com>
2025-10-18 11:21:33 +0530

    Tags

--- constants/routes.ts
+++ constants/routes.ts
@@ -9,7 +9,7 @@ const ROUTES = {
   JOBS: "/jobs",
   PROFILE: (id: string) => `/profile/${id}`,
   QUESTION: (id: string) => `/questions/${id}`,
-  TAG: (id: string) => `/tags/${id}`,
+  TAGS: (id: string) => `/tags/${id}`,
   SIGN_IN_WITH_OAUTH: `signin-with-oauth`,
 };
 
